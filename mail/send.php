b31598855d21aa76936e98450c69400d83e27554
Vinicius B. R <viniromano@gmail.com>
2019-11-19 00:05:40 -0300

    Adição de textos

---
+++ mail/send.php
@@ -0,0 +1,8 @@
+<?php
+    $email="contato@ame-lab.com";
+    $head = "From: " . $_POST['email'];
+    $headers = "Content-Type: text/html; charset=UTF-8";
+    $title= "Contato - " . $_POST['nome'];
+    $body = $_POST['mensagem'];
+    $success = mail($email, $title, $body, $head);
+?>

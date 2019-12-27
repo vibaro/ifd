509dc8e5f9b6ba820c7ded6691b9cf73775e25b9
Vinicius B. R <viniromano@gmail.com>
2019-12-27 14:57:19 -0300

    Contact page

---
+++ scripts/contact.js
@@ -0,0 +1,29 @@
+!function() {
+
+    const contact = {
+        init: function() {
+            contact.events();
+        },
+
+        events: function() {
+            $('form').delegate('#enviar', 'click', contact.sendMail);
+        },
+
+        sendMail: function() {
+            var data = {
+                nome: $.trim($('#nome').val()),
+                email: $.trim($('#email').val()),
+                mensagem: $.trim($('#mensagem').val())
+            };
+
+            if (data.nome && data.email && data.mensagem)
+                $.post('../mail/send.php', data, function() {
+                    alert('E-mail enviado! Aguarde nosso contato.');
+                });
+            }
+        }
+    };
+
+    contact.init();
+
+} ();
